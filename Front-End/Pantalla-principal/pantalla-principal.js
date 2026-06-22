70a6fe139b766bd1d44ca884c8bcd2db702df9ce
Axel-Wolkowicz <wolkowiczaxel@gmail.com>
2026-06-22 11:05:55 -0300

    tic xpeerience

--- Front-End/Pantalla-principal/pantalla-principal.js
+++ Front-End/Pantalla-principal/pantalla-principal.js
@@ -85,7 +85,9 @@ function toggleMode(){
       if (flechaL) flechaL.src = "../Imagenes/Tecla-l-clara.png";
       lucesEncendidas = !lucesEncendidas;
       controlManual = true;
-
+      cambiarLuz();
+    }
+    function cambiarLuz(){
       if (lucesEncendidas) {
         luzAdelante.style.display = "block";
         luzAtras.style.display = "block";
@@ -96,6 +98,38 @@ function toggleMode(){
         luzAtras.style.display = "none";
         postEvent("teclaL", {msg: "APAGAR"});
       }
+    }
+    luzAdelante.addEventListener("click", cambiarLuz);
+    luzAtras.addEventListener("click", cambiarLuz);
+    logo.addEventListener("click", fiesta);
+    function fiesta() {
+      postEvent("teclaL", { msg: "FIESTA" });
+    
+      const colors = [
+        "#ff0080", "#ff6600", "#ffff00", "#00ff00",
+        "#00ffff", "#0066ff", "#cc00ff", "#ff0055",
+        "#ff9900", "#00ff99", "#ff00ff", "#00ccff"
+      ];
+    
+      let i = 0;
+      const interval = setInterval(() => {
+        const c1 = colors[i % colors.length];
+        const c2 = colors[(i + 3) % colors.length];
+        const c3 = colors[(i + 6) % colors.length];
+    
+        document.body.style.background =
+          `linear-gradient(${i * 37}deg, ${c1}, ${c2}, ${c3})`;
+        document.body.style.transition = "background 0.1s ease";
+    
+        i++;
+      }, 120);
+    
+      setTimeout(() => {
+        clearInterval(interval);
+        document.body.style.background = "";
+        document.body.style.transition = "";
+        postEvent("teclaL", { msg: "APAGAR" });
+      }, 5000);
     }
     if (event.key === "ArrowUp" || event.key === "w" || event.key === "W") {
       console.log("Se presionó la flecha ↑");
